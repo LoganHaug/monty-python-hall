88abd1584a4e56fa2471ddb0f88e0a278612ab71
LoganHaug <logannhaug@gmail.com>
2021-04-23 22:57:03 -0700

    add the gameshow class

---
+++ gameshow.py
@@ -0,0 +1,12 @@
+class Gameshow:
+    def __init__(self):
+        pass
+
+    def _get_doors(self) -> dict:
+        pass
+
+    def switch_guess(self):
+        pass
+
+    def keep_guess(self):
+        pass
